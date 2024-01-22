d663cfb3ca92f9d36e380a4b53eebbdc0d427fd0
Herbert Duarte <herbertduarte.santos@gmail.com>
2024-01-22 16:56:02 -0300

    refactor route

--- src/routes.ts
+++ src/routes.ts
@@ -7,20 +7,13 @@ import { FindUserController } from "./modules/user/controllers/FindUserControlle
 import { UserAuthController } from "./modules/auth/controllers/UserAuthController";
 import { authGuard } from "./middlewares/AuthGuard";
 
-const createUserControler = new CreateUserController();
-const listUsersController = new ListUsersController();
-const updateUserController = new UpdateUserController();
-const deleteUserController = new DeleteUserController();
-const findUserController = new FindUserController();
-const userAuthController = new UserAuthController()
-
 const router = Router();
 
-router.post("/user", createUserControler.handle);
-router.get("/user", authGuard, listUsersController.handle);
-router.get("/user/:id", authGuard, findUserController.handle);
-router.put("/user/:id",authGuard, updateUserController.handle);
-router.delete("/user/:id",authGuard, deleteUserController.handle);
-router.post("/user/auth", userAuthController.handle);
+router.get("/user", authGuard, new ListUsersController().handle);
+router.get("/user/:id", authGuard, new FindUserController().handle);
+router.put("/user/:id", authGuard, new UpdateUserController().handle);
+router.delete("/user/:id", authGuard, new DeleteUserController().handle);
+router.post("/user", new CreateUserController().handle);
+router.post("/user/auth", new UserAuthController().handle);
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { router } from "./routes";
-import cors from "cors"
+import cors from "cors";
 
 const PORT = Number(process.env.PORT || 3000);
 const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(router);
 
 app.get("/", (request, response) => {
@@ -13,5 +13,15 @@ app.get("/", (request, response) => {
 });
 
 app.listen(PORT, () => {
+  console.log("\x1b[32m") 
+  listRoutes()
   console.log("🚀 HTTP server running on port " + PORT + "...");
+  console.log("\x1b[0m");
 });
+
+function listRoutes(){
+  router.stack.map((item)=>{
+    console.log(`[OK] ${item.route.stack[0].method.toLocaleUpperCase()} => ${item.route.path}`)
+  })
+  console.log()
+}

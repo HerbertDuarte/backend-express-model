5732db4788ec694b2516fbcd0134da43cfb2e0f5
Herbert Duarte <herbertduarte.santos@gmail.com>
2024-02-05 11:12:42 -0300

    change interval time on heart beat

--- src/middlewares/HeartBeat.ts
+++ src/middlewares/HeartBeat.ts
@@ -2,7 +2,7 @@ import logger from "@/utils/Loggers/logger";
 import { Request, Response, NextFunction } from "express";
 
 const minuto = 60 * 1000;
-const HEARTBEAT_INTERVAL = 1 * minuto;
+const HEARTBEAT_INTERVAL = 5 * minuto;
 let intervalId: NodeJS.Timeout;
 
 function HeartBeat(req: Request, res: Response, next: NextFunction) {

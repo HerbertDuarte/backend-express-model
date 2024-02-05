ba113c9d03c0a5b343d4d90c847b5e15e59d416d
Herbert Duarte <herbertduarte.santos@gmail.com>
2024-02-05 11:07:08 -0300

    fix interval time on heart beat

--- src/middlewares/HeartBeat.ts
+++ src/middlewares/HeartBeat.ts
@@ -1,13 +1,15 @@
+import logger from "@/utils/Loggers/logger";
 import { Request, Response, NextFunction } from "express";
 
 const minuto = 60 * 1000;
-const HEARTBEAT_INTERVAL = 0.1 * minuto;
+const HEARTBEAT_INTERVAL = 1 * minuto;
 let intervalId: NodeJS.Timeout;
 
 function HeartBeat(req: Request, res: Response, next: NextFunction) {
   if (!intervalId) {
     intervalId = setInterval(() => {
       fetch(process.env.SELF_URL, { method: "GET" });
+      logger.alert("Heart Beat");
     }, HEARTBEAT_INTERVAL);
   }
   next();
